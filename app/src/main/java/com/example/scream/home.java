273b6137bf1c69d2d710ee2c9a0f77c3cfb61f95
ashifReza <ashifcse247@gmail.com>
2020-08-14 00:27:38 +0600

    kkk

---
+++ app/src/main/java/com/example/scream/home.java
@@ -0,0 +1,36 @@
+package com.example.scream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class home extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnLogout = findViewById(R.id.button2);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent inToMain = new Intent(home.this,MainActivity.class);
+                startActivity(inToMain);
+            }
+        });
+
+    }
+}
